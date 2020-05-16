c472e742abe1a03a31efdb46ef515e622ac587e0
Alfio Puglisi <alfio.puglisi@inaf.it>
2020-05-16 15:17:48 +0200

    unit tests

---
+++ test/helper_test.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from guietta.guietta import _sequence, _mutable_sequence
+
+from PyQt5.QtWidgets import QWidget 
+
+class HelperTest(unittest.TestCase):
+    
+    def test_sequence(self):
+
+        ok_things = [ [1,2,3], ['a', 'b'], (1,2,3)]
+        ko_things = [ None, 'foo', 3.1415, {'a': 'dict'}]
+        
+        for thing in ok_things:
+            assert _sequence(thing) is True
+
+        for thing in ko_things:
+            assert _sequence(thing) is False
+
+    def test_mutable_sequence(self):
+
+        ok_things = [ [1,2,3], ['a', 'b'] ]
+        ko_things = [ (1,2,3), None, 'foo', 3.1415, {'a': 'dict'}]
+        
+        for thing in ok_things:
+            assert _mutable_sequence(thing) is True
+
+        for thing in ko_things:
+            assert _mutable_sequence(thing) is False
+            
--- test/init_test.py
+++ test/init_test.py
@@ -3,22 +3,21 @@
 import unittest
 from guietta import Gui
 
-# This line causes a core dump in pytest
-#from PyQt5.QtWidgets import QWidget 
+from PyQt5.QtWidgets import QWidget 
 
 class InitTest(unittest.TestCase):
     
-    def test_that_only_lists_of_lists_of_qt_are_accepted(self):
+    def test_that_only_lists_of_qt_widgets_are_accepted(self):
 
         things = [1, 'a', [1, 2], QWidget(), self, 3.1415, ('a', 'tuple')]        
         
         for thing in things:
-            with self.assertRaises(TypeError):
+            with self.assertRaises(ValueError):
                 print(thing)
                 a = Gui(thing)
             
         list_of_lists = [[QWidget(), QWidget()], [QWidget(), QWidget()]]
-        a = Gui(list_of_lists)    # Does not raise
+        a = Gui(*list_of_lists)    # Does not raise
 
         
 
