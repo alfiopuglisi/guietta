de85c0f0516c3ef871930a4b026ab304fe8c3d9f
Alfio Puglisi <alfio.puglisi@inaf.it>
2020-07-04 19:59:35 +0200

    Added signal property example

---
+++ guietta/examples/signal_property.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Demostrates how to use Gui properties
+# to connect a handler to a widget's default signal
+
+from guietta import _, ___, Gui, Quit, Exceptions
+        
+gui = Gui(
+    
+  [  'Enter numbers:', '__num1__' , '+' , '__num2__',  ['Calculate'] ],
+  [  'Result:  -->'  , 'result'   , ___ ,  ___      ,       _        ],
+  [  _               ,    _       ,  _  ,   _       ,      Quit      ],
+  exceptions = Exceptions.POPUP)
+
+
+def calc(gui, *args):
+    gui.result = float(gui.num1) + float(gui.num2)
+
+# Assign a callable to the button
+
+gui.Calculate = calc
+
+gui.run()
