46310dcfd64d9c202f19a2b4a4be84d113690ff8
Alfio Puglisi <alfio.puglisi@inaf.it>
2020-10-03 11:56:56 +0200

    Fixed unit test for normalize()

--- test/normalize_test.py
+++ test/normalize_test.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from guietta.guietta import _normalize
+from guietta.guietta import normalize
 
 
 class NormalizeTest(unittest.TestCase):
 
     def test_normal_chars(self):
         a = 'foobar'
-        assert _normalize(a) == a
+        assert normalize(a) == a
 
     def test_normal_chars2(self):
         a = 'FooBar_123'
-        assert _normalize(a) == a
+        assert normalize(a) == a
 
     def test_special_chars(self):
         a = 'Foo!Bar?-/4+`\\ 123:*'
-        assert _normalize(a) == 'FooBar4123'
+        assert normalize(a) == 'FooBar4123'
